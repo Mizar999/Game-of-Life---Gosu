2582184f9d86fb744edcd5f4f3b4c609c38da8c2
Mario S <mizar2502@aol.com>
2011-03-24 14:52:54 +0100

    added some methods to CellGridView

--- lib/cell_grid_view.rb
+++ lib/cell_grid_view.rb
@@ -1,10 +1,24 @@
 class CellGridView
   attr_accessor :x, :y, :cell_width, :cell_height
+  attr_accessor :color_alive, :color_dead, :color_revived
   
   def initialize(params = Hash.new)
     @x = params[:x] || 0
     @y = params[:y] || 0
     @cell_width = params[:cell_width] || 0
     @cell_height = params[:cell_height] || 0
+    @cell_grid = params[:cell_grid] || CellGrid.new
+
+    @color_alive = params[:color_alive] || 0xff000000
+    @color_dead = params[:color_dead] || 0xff000000
+    @color_revived = params[:color_revived] || 0xff000000
+  end
+
+  def total_width
+    @cell_grid.columns * @cell_width
+  end
+
+  def total_height
+    @cell_grid.rows * @cell_height
   end
 end
--- test/test_cell_grid_view.rb
+++ test/test_cell_grid_view.rb
@@ -1,8 +1,12 @@
-file = "../lib/cell_grid_view"
+files = ["../lib/cell_grid_view", "../lib/cell_grid"]
 begin
-  require_relative file
+  files.each do |filename|
+    require_relative filename
+  end
 rescue NoMethodError
-  require file
+  files.each do |filename|
+    require filename
+  end
 end
 
 require "test/unit"
@@ -37,6 +41,22 @@ class TestCellGridView < Test::Unit::TestCase
     @grid_view = CellGridView.new(:x => 40, :y => 34)
     assert_grid_position(40, 34)
   end
+
+  def test_total_width_and_height
+    cell_grid = CellGrid.new(:rows => 3, :columns => 3)
+    @grid_view = CellGridView.new(:cell_grid => cell_grid, :cell_width => 20, :cell_height => 10)
+    assert_total_width_and_height(60, 30)
+    cell_grid.rows = 5
+    cell_grid.columns = 4
+    assert_total_width_and_height(80, 50)
+  end
+
+  def test_cell_colors
+    @grid_view = CellGridView.new(:color_alive => 0xffffff00, :color_dead => 0xff00ffff, :color_revived => 0xffff00ff)
+    assert_color_alive_dead_revived(0xffffff00, 0xff00ffff, 0xffff00ff)
+    set_color_alive_dead_revived(0xff0000ff, 0xffff0000, 0xff00ff00)
+    assert_color_alive_dead_revived(0xff0000ff, 0xffff0000, 0xff00ff00)
+  end
   
   private
   
@@ -49,6 +69,12 @@ class TestCellGridView < Test::Unit::TestCase
     @grid_view.x = x
     @grid_view.y = y
   end
+
+  def set_color_alive_dead_revived(color_alive, color_dead, color_revived)
+    @grid_view.color_alive = color_alive
+    @grid_view.color_dead = color_dead
+    @grid_view.color_revived = color_revived
+  end
   
   def assert_cell_dimensions(cell_width, cell_height)
     assert_equal(cell_width, @grid_view.cell_width)
@@ -58,5 +84,16 @@ class TestCellGridView < Test::Unit::TestCase
   def assert_grid_position(x, y)
     assert_equal(x, @grid_view.x)
     assert_equal(y, @grid_view.y)
+  end
+
+  def assert_total_width_and_height(total_width, total_height)
+    assert_equal(total_width, @grid_view.total_width)
+    assert_equal(total_height, @grid_view.total_height)
+  end
+
+  def assert_color_alive_dead_revived(color_alive, color_dead, color_revived)
+    assert_equal(color_alive, @grid_view.color_alive)
+    assert_equal(color_dead, @grid_view.color_dead)
+    assert_equal(color_revived, @grid_view.color_revived)
   end
 end
